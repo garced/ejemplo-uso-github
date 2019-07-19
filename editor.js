57d70efa5f4c58dc76cea0fc4b185585f9d86fa4
Guillermo Arce <garce@MacBook-Air-de-Guillermo.local>
2019-07-18 23:16:29 -0400

    Agregamos sitio general

---
+++ editor.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#summernote').summernote(
+        
+    );
+});
+
+var edit = function() {
+    $('.click2edit').summernote({focus: true});
+};
+
+var save = function() {
+    var markup = $('.click2edit').summernote('code');
+        $('.click2edit').summernote('destroy');
+};
